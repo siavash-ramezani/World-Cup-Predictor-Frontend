e1ce1c2e915ee46f37bbb032652bb8fc410f5c3e
s.ramezani <siavash.ramezani73@gmail.com>
2026-07-08 11:03:42 +0330

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "fm6.ir",
+                pathname: "/avatars/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
